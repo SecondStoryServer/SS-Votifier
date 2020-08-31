536498e0fc092a27b69c8b782578eb0cbd84ab6a
sya-ri <sya79lua@gmail.com>
2020-09-01 02:55:52 +0900

    Kotlin 1.4.0, Paper 1.16.2 に対応

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
     `maven-publish`
 }
 
 group = "me.syari.ss.votifier"
-version = "1.1.2"
+version = "1.1.3"
 
 val ssMavenRepoURL: String by extra
 val ssMavenRepoUploadURL: String by extra
@@ -23,9 +23,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.destroystokyo.paper:paper-api:1.16.1-R0.1-SNAPSHOT")
-    implementation("me.syari.ss.core:SS-Core:3.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.destroystokyo.paper:paper-api:1.16.2-R0.1-SNAPSHOT")
+    implementation("me.syari.ss.core:SS-Core:3.1.1")
+    implementation(kotlin("stdlib-jdk8"))
     implementation("io.netty:netty-handler:4.1.49.Final")
     implementation("io.netty", "netty-transport-native-epoll", "4.1.49.Final", classifier = "linux-x86_64")
 }
